c355ef2875e252e84e9ddd68cf6525ad8360e7d6
sonicj <sonicjhon56@gmail.com>
2026-04-08 11:48:15 +0700

    feat: Auto update yt-dlp to latest nightly

--- packages/sealed_dioxus_ui/src/main.rs
+++ packages/sealed_dioxus_ui/src/main.rs
@@ -22,7 +22,45 @@ fn main() {
 
         DATABASE.try_initialize_storage::<DownloadItem, Vec<DownloadItem>>(vec![])?;
 
-        YT_DLP_LIBRARIES.install_dependencies().await?;
+        if YT_DLP_LIBRARIES.ffmpeg.exists() && YT_DLP_LIBRARIES.youtube.exists() {
+            if let Ok(metadata) = YT_DLP_LIBRARIES.youtube.metadata()
+                && let Ok(modified) = metadata.modified()
+                && let Ok(elapsed) = modified.elapsed()
+            {
+                if elapsed >= std::time::Duration::from_hours(12) {
+                    info!(
+                        "Updating yt-dlp to latest nightly at: ({})",
+                        std::path::absolute(&YT_DLP_LIBRARIES.youtube)?.display()
+                    );
+
+                    if !std::process::Command::new(&YT_DLP_LIBRARIES.youtube)
+                        .args(["--update", "--update-to", "nightly"])
+                        .status()?
+                        .success()
+                    {
+                        error!("Failed to update yt-dlp to latest nightly");
+                    }
+                }
+            } else {
+                warn!("Failed to check if yt-dlp needs an update");
+            };
+        } else {
+            info!(
+                "Installing ffmpeg at: ({}) and yt-dlp at: ({})",
+                std::path::absolute(&YT_DLP_LIBRARIES.ffmpeg)?.display(),
+                std::path::absolute(&YT_DLP_LIBRARIES.youtube)?.display()
+            );
+
+            YT_DLP_LIBRARIES.install_dependencies().await?;
+
+            if !std::process::Command::new(&YT_DLP_LIBRARIES.youtube)
+                .args(["--update", "--update-to", "nightly"])
+                .status()?
+                .success()
+            {
+                error!("Failed to update yt-dlp to latest nightly");
+            }
+        }
 
         Ok(dioxus::server::router(App))
     });
